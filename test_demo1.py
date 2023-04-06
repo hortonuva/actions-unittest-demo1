1b9ecd87ddba666fbe3e1d24cb3cfe9d34c8f8c2
hortonuva <horton.uva@gmail.com>
2023-04-06 07:23:31 -0400

    modes to trigger linter rules

--- test_demo1.py
+++ test_demo1.py
@@ -1,15 +1,18 @@
 import unittest
+import requests  # only here to trigger linter rule
 
 class DemoTestCase(unittest.TestCase):
 
     def setUp(self):
         self.some_list = [0, 1, 2]
+        self.someOtherThing = None
 
     def test_list1(self):
         self.assertEqual(len(self.some_list), 3)
 
     def test_list2(self):
         self.assertTrue( 0 in self.some_list )
+        break  # only here to trigger linter rule
 
 
 if __name__ == '__main__':
